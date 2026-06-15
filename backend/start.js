88f65079f7a5ec1c5785de770e32fbfda3b44dd0
Nephia <137403442+Nephiau@users.noreply.github.com>
2026-06-15 12:52:03 +0700

    fgh class update data

--- backend/start.js
+++ backend/start.js
@@ -2,5 +2,6 @@ const { execSync } = require('child_process')
 
 execSync('node backend/seedAdmin.js', { stdio: 'inherit' })
 execSync('node backend/seedSiswa.js', { stdio: 'inherit' })
+execSync('node backend/seedFGH.js', { stdio: 'inherit' })
 
 require('./index.js')
